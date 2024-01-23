a716d9151c126744690fefe4845b379d7c9b9e9c
eyob-kefale <eyobkefale1@gmail.com>
2024-01-23 20:59:05 +0300

    add application server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
-# Setting upper limit for the pinging requests
-exec { 'upperlimit' :
-  command =>  'sed -i "s/15/10000/g" /etc/default/nginx;service nginx restart',
-  path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  =>  'test -e /etc/default/nginx',
+# Set ulimit on nginx max file descriptors to system soft limit
+exec {'ulimit':
+  command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -e /etc/default/nginx',
+}
+
+exec {'restart nginx':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['ulimit'],
 }
